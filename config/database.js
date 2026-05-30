abacd95eceded7beac87f35af3395ccd7ae589d0
abdallahgh04 <168266476+abdallahgh04@users.noreply.github.com>
2026-05-30 21:32:50 +0200

    01

--- config/database.js
+++ config/database.js
@@ -57,22 +57,12 @@ module.exports = ({ env }) => {
       connection: {
         connectionString: env("DATABASE_URL"),
         host: env("DATABASE_HOST"),
-        port: env.int("DATABASE_PORT"),
+        port: env.int("DATABASE_PORT", 5432),
         database: env("DATABASE_NAME"),
         user: env("DATABASE_USERNAME"),
         password: env("DATABASE_PASSWORD"),
         ssl: env.bool("DATABASE_SSL", false)
-          ? {
-              key: env("DATABASE_SSL_KEY", undefined),
-              cert: env("DATABASE_SSL_CERT", undefined),
-              ca: env("DATABASE_SSL_CA", undefined),
-              capath: env("DATABASE_SSL_CAPATH", undefined),
-              cipher: env("DATABASE_SSL_CIPHER", undefined),
-              rejectUnauthorized: env.bool(
-                "DATABASE_SSL_REJECT_UNAUTHORIZED",
-                false
-              ),
-            }
+          ? { rejectUnauthorized: false }
           : false,
         schema: env("DATABASE_SCHEMA", "public"),
       },
